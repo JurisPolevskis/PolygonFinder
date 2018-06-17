3345b9f4049d7382cb3b4f499e9e2174b0d72160
Juris.Polevskis <jpolev@gmail.com>
2018-06-17 10:13:35 +0300

    Added main.c prototype

---
+++ makefile
@@ -0,0 +1,34 @@
+#based on http://www.partow.net/programming/makefile/index.html
+
+CXX      := g++
+CXXFLAGS := -std=c++11 -Wall -Werror -g
+#CXXFLAGS += -pedantic-errors -Wextra #Add these when basic funcions implemented
+LDFLAGS  := -L/usr/lib -lstdc++ -lm
+BUILD    := ./build
+OBJ_DIR  := $(BUILD)/objects
+APP_DIR  := $(BUILD)/bin
+TARGET   := polygf
+INCLUDE  := -Isrc/
+SRC      := $(wildcard src/*.cpp)
+
+OBJECTS := $(SRC:%.cpp=$(OBJ_DIR)/%.o)
+
+all: build $(APP_DIR)/$(TARGET)
+
+$(OBJ_DIR)/%.o: %.cpp
+	@mkdir -p $(@D)
+	$(CXX) $(CXXFLAGS) $(INCLUDE) -o $@ -c $<
+
+$(APP_DIR)/$(TARGET): $(OBJECTS)
+	@mkdir -p $(@D)
+	$(CXX) $(CXXFLAGS) -o $(APP_DIR)/$(TARGET) $(OBJECTS) $(INCLUDE) $(LDFLAGS) 
+
+.PHONY: all build clean
+
+build:
+	@mkdir -p $(APP_DIR)
+	@mkdir -p $(OBJ_DIR)
+
+clean:
+	-@rm -rvf $(OBJ_DIR)/*
+	-@rm -rvf $(APP_DIR)/*
---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+//#include "ArgParser.h"
+//#include "FileParser.h"
+//#include "Line.h"
+//#include "Intersector.h"
+//#include "Graph.h"
+//#include "PolygonProcessor.h"
+
+using namespace std;
+
+void printUsage() {
+	cout << "TODO:Document usage";
+}
+
+int main(int argc, char **argv){
+	//ArgParser args(argc, argv);
+	//if(args.cmdOptionExists("-h") || args.cmdOptionExists("--help")){
+	//	printUsage();
+	//	return 0;
+	//}
+	//const string &in_filename = args.getCmdOption("-f");
+	//const string &out_filename = args.getCmdOption("-o");
+	//const string &delimiters = args.getCmdOption("-d");
+	//if (!in_filename.empty()){
+		//FileParser input_csv(in_filename);
+		//lines_t lines = input_csv.getLines(delimiters);
+		//Intersector intersector(lines);
+		//Graph graph(intersector.getIntersections());
+		//PolygonProcessor processor(graph.getCycles(), lines);
+		//processor.discardZeroLengthSides();
+		//processor.discardZeroAreaPolygons();
+		//Output output(out_filename);
+		//output.print(processor.printPolygons());
+	//}
+	//else {
+	//	printUsage();
+	//}
+    return 0;
+}
