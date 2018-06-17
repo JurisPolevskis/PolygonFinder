540c80b7603f803e70ad390a0ab9d9166ceb53de
Juris.Polevskis <jpolev@gmail.com>
2018-06-17 20:37:30 +0300

    Implemented makeLines

--- src/Line.cpp
+++ src/Line.cpp
@@ -1,5 +1,12 @@
 #include <Line.h>
 
+#include <Debug.h>
+
+Line::Line()
+{
+	//Nothing
+}
+
 Line::Line(const Point& start, const Point& end)
 {
 	c_start = start;
@@ -24,9 +31,43 @@ Point getIntersectionPoint(const Line& line)
 	return Point(0, 0);
 }
 
-lines_t makeLines( std::vector< std::vector<coordinate_t> > )
+std::pair<Point, Point> Line::getEnds()
 {
-	//TODO:Implement
+	return std::make_pair(c_start, c_end);
+}
+
+std::string to_string(Line line)
+{
+	auto ends = line.getEnds();
+	std::string str = Point::to_string(ends.first) + ", " + Point::to_string(ends.second);
+	return str;
+}
+
+lines_t makeLines( std::vector< std::vector<coordinate_t> > table)
+{
 	lines_t lines;
+	for (const auto& table_line:table) {
+		if (table_line.size() != 5) {
+			throw std::invalid_argument( "makeLines invalid value caount in line" );
+		}
+		Point start(table_line[1], table_line[2]);
+		Point end(table_line[1], table_line[2]);
+		line_id_t line_id = table_line[0];
+		Line line(start, end);
+		lines[line_id] = line;
+	}
+	Debug::print("makeLines returns:");
+	Debug::print(linesToString(lines));
 	return lines;
+}
+
+std::string linesToString(lines_t lines)
+{
+	std::string message = "Lines:";
+	for (const auto& entry:lines) {
+		message.append("\n " + std::to_string(entry.first) +
+				+ ":\t" + to_string(entry.second) );
+		
+	}
+	return message;
 }
--- src/Line.h
+++ src/Line.h
@@ -9,14 +9,18 @@ typedef int line_id_t;
 class Line
 {
 	public:
+		Line();
 		Line(const Point& start, const Point& end);
 		bool isIntersecting(const Line& line);
 		bool isTouching(const Point& point);
 		Point getIntersectionPoint(const Line& line);
+		std::pair<Point, Point> getEnds();
+		static std::string to_string(Point point);
 	private:
 		Point c_start;
 		Point c_end;
 };
 
 typedef std::map<line_id_t, Line> lines_t;
-lines_t makeLines(std::vector<std::vector<coordinate_t>>);
+lines_t makeLines( std::vector< std::vector<coordinate_t> > table);
+std::string linesToString(lines_t lines);
