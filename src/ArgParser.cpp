0839641e4603653dcb10de50c48e46e1445bf6f6
Juris.Polevskis <jpolev@gmail.com>
2018-06-17 17:20:52 +0300

    Implemented simple ArgParser

--- src/ArgParser.cpp
+++ src/ArgParser.cpp
@@ -1,6 +1,7 @@
 #include <ArgParser.h>
 
 #include <iostream>
+#include <algorithm>
 
 void test() {
 	std::cout << "test OK";
@@ -15,13 +16,15 @@ ArgParser::ArgParser(const int& argc, char **argv)
 
 bool ArgParser::cmdOptionExists(const std::string& option)
 {
-	//TODO:Implement
-	return false;
+	return (std::find(c_tokens.begin(), c_tokens.end(), option) != c_tokens.end());
 }
 
 const std::string& ArgParser::getCmdOption(const std::string& option)
 {
-	//TODO:Implement
+	auto it = std::find(c_tokens.begin(), c_tokens.end(), option);
+	if (it != c_tokens.end() && (it + 1) != c_tokens.end()) {
+		return *(it + 1);
+	}
 	static const std::string empty_string = "";
 	return empty_string;
 }
