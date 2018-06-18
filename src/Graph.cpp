7be3fafdb470ca25a061799e2071fd4a75bc6083
Juris.Polevskis <jpolev@gmail.com>
2018-06-18 12:58:33 +0300

    Incorrect implementation of Graph::calculateCycles

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -1,8 +1,10 @@
-#include <Graph.h>
+#include "Graph.h"
+
+#include "Debug.h"
 
 Graph::Graph(const intersections_t& intersections)
 {
-	this->cycles = calculateCycles(intersections);
+	calculateCycles(intersections);
 }
 
 const cycles_t& Graph::getCycles()
@@ -10,8 +12,63 @@ const cycles_t& Graph::getCycles()
 	return this->cycles;
 }
 
-cycles_t Graph::calculateCycles(const intersections_t& intersections)
+
+//Depth first search with backtracking while storing visited nodes
+//One pass starting for every line that has an intersection
+void Graph::calculateCycles(const intersections_t& intersections)
 {
-	cycles_t cycles;
-	return cycles;
+	for (const auto& it:intersections) {
+		Debug::print("x");
+		auto key = &it.first;
+		visited_nodes.clear();
+		this->current_start_node = key;
+		Debug::print("Start Node (" + std::to_string(key->first) + ", " + std::to_string(key->second) + ")");
+		visitNode(current_start_node, intersections);
+	}
+	Debug::print(cyclesToString(this->cycles));
+}
+
+void Graph::visitNode(const intersection_key_t *node, const intersections_t& intersections) {
+	Debug::print("Depth:" + std::to_string(current_path.size()));
+	Debug::print("Visiting Node (" + std::to_string(node->first) + ", " + std::to_string(node->second) + ")");
+	visited_nodes.insert(node);
+	current_path.insert(node);
+	//const auto& next_nodes = ;
+	for (const auto& it:intersections) {
+		auto next_node = &it.first;
+		Debug::print("Next Node (" + std::to_string(next_node->first) + ", " + std::to_string(next_node->second) + ")");
+		//If return to start add to cycle
+		if (next_node == current_start_node && current_path.size() > 2) {
+			Debug::print("Start found");
+			cycles.insert(current_path);
+		}
+		//If new node visit it
+		else if (current_path.find(next_node) == current_path.end()) {
+			visitNode(next_node, intersections);
+		}
+	}
+	current_path.erase(node);
+}
+
+std::string Graph::cycleToString(const cycle_t& cycle){
+	std::string message = "(";
+	bool first_value = true;
+	for (const auto& it:cycle) {
+		if (!first_value) {
+			message.append("->");
+		}
+		first_value = true;
+		message.append( "(" + std::to_string(it->first) + ", " + std::to_string(it->second) + ")");
+	}
+	message.append(")");
+	return message;
+}
+
+
+std::string Graph::cyclesToString(const cycles_t& cycle){
+	std::string message = "Cycles:";
+	for (const auto& cycle:cycles) {
+		message.append("\n" + cycleToString(cycle));
+	}
+	return message;
 }
--- src/Graph.h
+++ src/Graph.h
@@ -3,7 +3,7 @@
 #include <set>
 #include "Intersector.h"
 
-typedef std::set<line_id_t> cycle_t;
+typedef std::set<const intersection_key_t*> cycle_t;
 typedef std::set<cycle_t> cycles_t;
 
 class Graph
@@ -11,7 +11,13 @@ class Graph
 	public:
 		Graph(const intersections_t& intersections);
 		const cycles_t& getCycles();
+		std::string cycleToString(const cycle_t& cycle);
+		std::string cyclesToString(const cycles_t& cycle);
 	private:
-		cycles_t calculateCycles(const intersections_t& intersections);
+		void calculateCycles(const intersections_t& intersections);
+		void visitNode(const intersection_key_t* node, const intersections_t& intersections);
 		cycles_t cycles;
+		std::set< const intersection_key_t* > visited_nodes;
+		std::set< const intersection_key_t* > current_path;
+		const intersection_key_t* current_start_node;
 };
